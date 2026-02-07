4f5a93fe0d7e376681730f0be863a86c308a4dca
ItzPopelka <popelka_dev@seznam.cz>
2026-02-07 05:51:29 -0800

    comments for easy use

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# dont change anything in imports or you can (90% sure) break the bot
 import discord
 from discord.ext.commands import *
 from discord.ext import commands
@@ -5,10 +6,29 @@ import asyncio
 import os
 import json
 
+# =======================================
+# ========== DISCORD BOT TOKEN ==========
+# =======================================
+# 1. Create a file named "token.json" in the same directory as this script.
+
+# 2. Add the following content to "token.json":
+# {"TOKEN": "YOUR_BOT_TOKEN"}
+
+# 3. Replace "YOUR_BOT_TOKEN" with your actual Discord bot token.
+
+# 4. Make sure to add "token.json" to your .gitignore file to prevent it from being uploaded to version control.
 token = json.load(open("token.json", "r"))["TOKEN"]
 
+# =======================================
+# ============ BOT SETTINGS =============
+# =======================================
+# intents are required for the bot to function properly, especially if you want to access certain events or data.
 intents = discord.Intents.all()
+
+# choose a command prefix for your bot (e.g., "r?" in this case + just make sure its not too common to avoid conflicts with other bots)
 bot = commands.Bot(command_prefix='r?', intents=intents)
+
+# just a simple way to remove the default help command so you can create your own custom one later if you want
 bot.remove_command('help')
 
 @bot.event
@@ -19,12 +39,11 @@ async def on_ready():
         activity=discord.Game(name="popelka je boss")
     )
 
-import base64
-def encrypt_token(token):
-    return base64.b64encode(token.encode()).decode()
-def decrypt_token(encrypted):
-    return base64.b64decode(encrypted.encode()).decode()
 
+# =======================================
+# ============ FUNCS LOADER =============
+# =======================================
+# just a semi-simple way to load all the command files in the "funcs" directory without having to import them manually one by one
 async def load_extensions():
     funcs_dir = "funcs"
     extensions = [f[:-3] for f in os.listdir(funcs_dir)
@@ -37,6 +56,10 @@ async def load_extensions():
         except Exception as e:
             print(f"error while loading {ext}: {e}")
 
+# =======================================
+# ============= BOT START ===============
+# =======================================
+# dont change anything here, this is just the code to start the bot and load the loaded extensions
 async def main():
     async with bot:
         await load_extensions()
