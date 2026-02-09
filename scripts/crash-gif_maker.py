ebd8f25b69cec9a52256e06251222ef7b32df2db
ItzPopelka <popelka_dev@seznam.cz>
2026-02-09 22:47:36 +0100

    nothing

--- scripts/crash-gif_maker.py
+++ scripts/crash-gif_maker.py
@@ -4,4 +4,4 @@ size = (15000, 15000)
 img = Image.new('RGB', size, color='black')
 
 img.save('crash.gif', 'GIF')
-print("File is ready. What is bigger? This file or your mom?")
+print("File is ready. What is bigger? This file or your mom? Idk bro.")
